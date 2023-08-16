99938aa3a0ccae7118dd8d8e369b243e7352e7b9
matteo <matteo.razzanelli89@gmail.com>
2023-08-16 17:28:07 +0200

    error, loader, seciton title components

---
+++ src/components/Loading.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import loadingGif from "../images/loader.gif";
+
+export default function Loading() {
+  return (
+    <div className="loading">
+      <h4>Recipes data loading....</h4>
+      <img src={loadingGif} alt="" />
+    </div>
+  );
+}
--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
 
 export default function Error() {
   return (
-    <div>Error Page</div>
+    <Hero>
+      <Banner title="404" subtitle="page not found">
+        <Link to="/" className="btn-primary">
+          RETURN HOME
+        </Link>
+      </Banner>
+    </Hero>
   )
 }
