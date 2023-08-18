82c788f21ec8922f43e4ba29a8b2e0173c084c83
matteo <matteo.razzanelli89@gmail.com>
2023-08-18 14:06:53 +0200

    random recipes components and its slice using redux toolkit

--- src/components/RandomRecipes.js
+++ src/components/RandomRecipes.js
@@ -1,7 +1,54 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+
+import { useDispatch, useSelector } from 'react-redux';
+import { 
+  selectAllRandomRecipes,
+  getRandomrecipesStatus,
+  getRandomrecipesError,
+  fetchRandomRecipes
+ } from '../features/randomRecipesSlice';
+
+import Title from "./Title";
+import Loading from "./Loading";
+import RecipeCard from "./RecipeCard"
 
 export default function RandomRecipes() {
+
+  const dispatch = useDispatch();
+  
+  const randomRecipes = useSelector(selectAllRandomRecipes);
+  const randomRecipesStatus = useSelector(getRandomrecipesStatus);
+  const randomRecipesError = useSelector(getRandomrecipesError);
+  
+  useEffect(()=>{
+    console.log('inside use effect : ', randomRecipesStatus)
+    if(randomRecipesStatus === 'idle'){
+      dispatch(fetchRandomRecipes()) // FIXME: called twice
+    }
+    console.log('recipes : ',randomRecipes)
+  },[randomRecipesStatus, dispatch])
+
+  let content;
+  if (randomRecipesStatus === 'loading') {
+    content = <Loading/>;
+  } else if (randomRecipesStatus === 'succeeded') {
+    console.log('single recipeeeeeeeeeeeeeeeeeeee')
+    content = randomRecipes.map(
+      (item) => {
+        return <RecipeCard key={item.id} title={item.title} id={item.id} image={item.image}/>
+      }
+    )
+  } else if (randomRecipesStatus === 'failed') {
+    content = <p>{randomRecipesError}</p>;
+  }
+
   return (
-    <div>RandomRecipes</div>
-  )
+    <div>
+      <Title title="Today's chef's tips" />
+      <section className="recipe-container">
+          {content}
+      </section>
+    </div>
+    
+  );
 }
---
+++ src/components/RecipeCard.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+export default function RecipeCard({ title, id, image }) {
+  return (
+    <article className="recipe-card">
+      <div className="img-container">
+        <img src={image || "../img/defaultImg.jpg"} alt="" />
+
+        <Link to={`/recipes/${id}`} className="btn-primary recipe-link">
+          More
+        </Link>
+      </div>
+      <p className="recipe-title">{title}</p>
+    </article>
+  )
+}
+
+RecipeCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
+  image: PropTypes.string.isRequired,
+};
