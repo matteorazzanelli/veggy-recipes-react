a053c4abfaba508e1c893cce6af32940e78a1ff9
matteo <matteo.razzanelli89@gmail.com>
2023-08-20 16:46:51 +0200

    modify single recipe component to check also inside the searched recipes stored list

--- src/pages/SingleRecipe.js
+++ src/pages/SingleRecipe.js
@@ -3,8 +3,10 @@ import { useParams } from "react-router-dom";
 
 import { useSelector } from 'react-redux';
 import { selectAllRandomRecipes } from '../features/randomRecipesSlice';
+import { selectAllSearchedRecipes } from "../features/searchedRecipesSlice";
 
 import Error from "./Error";
+import Loading from "../components/Loading";
 
 import styled from "styled-components"
 
@@ -13,58 +15,73 @@ export default function SingleRecipe() {
   let params = useParams();
 
   const randomRecipes = useSelector(selectAllRandomRecipes);
+  const searchedRecipes = useSelector(selectAllSearchedRecipes);
 
   const [activeRecipe, setActiveRecipe] = useState({});
   const [activeTab, setActiveTab] = useState("instructions");
 
   useEffect(()=>{
     const searchRecipe = () => {
-      const matchedRecipes = randomRecipes.filter((recipe)=>{
+      // search in teh random and filtered recipes
+      let matchedRecipes = randomRecipes.filter((recipe)=>{
         if(recipe.id === parseInt(params.id))
           return recipe;
       })
+      matchedRecipes = matchedRecipes.concat(searchedRecipes.filter((recipe)=>{
+        if(recipe.id === parseInt(params.id))
+          return recipe;
+      }))
+      console.log('match : ',matchedRecipes[0]);
       setActiveRecipe(matchedRecipes[0] ?? null)
     }
     searchRecipe()
-  },[])
+  },[params.id, randomRecipes, searchedRecipes, ])
 
   if(activeRecipe == null){
     return (<Error/>)
   }
-  return (    
-    <DeatilsWrapper>
+
+  return (
+    <>   
+      <DeatilsWrapper>
+        <div>
+          <h2>{activeRecipe.title}</h2>
+          <img src={activeRecipe.image} alt=""/>
+        </div>
+        <Info>
+          <Button 
+            className={activeTab === 'instructions' ? 'active' : ''}
+            onClick={()=>setActiveTab('instructions')}
+            >
+              Instructions
+          </Button>
+          <Button 
+            className={activeTab === 'ingredients' ? 'active' : ''}
+            onClick={()=>setActiveTab('ingredients')}
+            >
+              Ingredients
+          </Button>
+          {activeTab === 'instructions' && (
+            <div>
+              <h3 dangerouslySetInnerHTML={{__html: activeRecipe.summary}}></h3>
+              <h3 dangerouslySetInnerHTML={{__html: activeRecipe?.instructions}}></h3>
+            </div>
+          )}
+          {activeTab === 'ingredients' && (
+            <ul>
+              {activeRecipe.extendedIngredients?.map((ingredient)=>(
+                <li>{ingredient.original}</li>
+              ))}
+            </ul>
+          )}
+        </Info>
+      </DeatilsWrapper>
       <div>
-        <h2>{activeRecipe.title}</h2>
-        <img src={activeRecipe.image} alt=""/>
+        <a href={activeRecipe.sourceUrl} target="_blank">
+          <button className="btn-primary">More Info</button>
+        </a>
       </div>
-      <Info>
-        <Button 
-          className={activeTab === 'instructions' ? 'active' : ''}
-          onClick={()=>setActiveTab('instructions')}
-          >
-            Instructions
-        </Button>
-        <Button 
-          className={activeTab === 'ingredients' ? 'active' : ''}
-          onClick={()=>setActiveTab('ingredients')}
-          >
-            Ingredients
-        </Button>
-        {activeTab === 'instructions' && (
-          <div>
-            <h3 dangerouslySetInnerHTML={{__html: activeRecipe.summary}}></h3>
-            <h3 dangerouslySetInnerHTML={{__html: activeRecipe.instructions}}></h3>
-          </div>
-        )}
-        {activeTab === 'ingredients' && (
-          <ul>
-            {activeRecipe.extendedIngredients.map((ingredient)=>(
-              <li key={ingredient.id}>{ingredient.original}</li>
-            ))}
-          </ul>
-        )}
-      </Info>
-    </DeatilsWrapper>
+    </>
   )
 }
 
