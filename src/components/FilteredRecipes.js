0a821626f4e6e773c61b1efa3d18b82268a61ea4
matteo <matteo.razzanelli89@gmail.com>
2023-08-20 16:45:56 +0200

    showing filtered recipes

--- src/components/FilteredRecipes.js
+++ src/components/FilteredRecipes.js
@@ -1,7 +1,42 @@
 import React from 'react'
 
+import { useSelector } from 'react-redux'
+import {
+  selectAllSearchedRecipes,
+  getSearchedRecipesStatus,
+  getSearchedRecipesError,
+} from '../features/searchedRecipesSlice';
+
+import Title from "./Title";
+import Loading from "./Loading";
+import RecipeCard from "./RecipeCard"
+
 export default function Filteredrecipes() {
+
+  const searchedRecipes = useSelector(selectAllSearchedRecipes);
+  const searchedRecipesStatus = useSelector(getSearchedRecipesStatus);
+  const searchedRecipesError = useSelector(getSearchedRecipesError);
+
+  let content;
+  if (searchedRecipesStatus === 'loading') {
+    content = <Loading/>;
+  } else if (searchedRecipesStatus === 'succeeded') {
+    console.log(searchedRecipes)
+    content = searchedRecipes.map(
+      (item) => {
+        return <RecipeCard key={item.id} title={item.title} id={item.id} image={item.image}/>
+      }
+    )
+  } else if (searchedRecipesStatus === 'failed') {
+    content = <p>{searchedRecipesError}</p>;
+  }
+
   return (
-    <div>Filteredrecipes</div>
+    <div>
+      <Title title="Results" />
+      <section className="recipe-container">
+          {content}
+      </section>
+    </div>
   )
 }
